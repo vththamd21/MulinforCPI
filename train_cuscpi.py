6fac81ff902f8ec321b3cc8efcf7e080ab69f725
Vũ Hồng Thắm <vuhongtham@MacBook-Air-cua-Vu-4.local>
2026-04-17 12:37:51 +0700

    dglbackkend

--- train_cuscpi.py
+++ train_cuscpi.py
@@ -2,6 +2,9 @@ import collections
 import collections.abc
 for type_name in collections.abc.__all__:
     setattr(collections, type_name, getattr(collections.abc, type_name))
+    
+import os
+os.environ['DGLBACKEND'] = 'pytorch'
 
 import argparse
 import concurrent.futures
